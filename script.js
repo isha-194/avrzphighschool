c73189f687ee7fad81eff10e55a535ce50355290
isha-194 <shaiksirisha33@gmail.com>
2024-10-01 05:49:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.getElementById("menu-icon");
+    const navMenu = document.getElementById("nav-menu");
+  
+    menuIcon.addEventListener("click", function () {
+      navMenu.classList.toggle("show-menu");
+    });
+  });
+  
